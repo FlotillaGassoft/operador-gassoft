88ba2e906a6be92d6b14b6d725936b4589a28b7a
FlotillaGassoft <108635956+FlotillaGassoft@users.noreply.github.com>
2022-07-30 20:03:33 -0500

    Update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  BASE_API_URL: "https://facturacion.gassoft.com.mx/flotilla-foranea/api"
+  BASE_API_URL: "https://test-operator-api.herokuapp.com/api"
 };
